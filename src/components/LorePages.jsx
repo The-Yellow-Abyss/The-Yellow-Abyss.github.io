230aef21f39a1fcd2754037818a1eb7fac9eb387
Cristian Pio Prudenzano <146067966+Cristian-Pio@users.noreply.github.com>
2025-05-30 22:55:29 +0200

    Lore  codex

--- src/components/LorePages.jsx
+++ src/components/LorePages.jsx
@@ -1,75 +1,107 @@
 import React from "react";
+import logoGif from '../assets/LogoBelloCheLampeggia.gif'; // regola il path se necessario
+
 
 export const Page1 = () => (
   <div>
-    <h1>Pagina 1</h1>
-    <p>Testo segnaposto per la pagina 1.</p>
+    <h1>Accadimenti recenti</h1>
+    <p>Nel 26 anno dell’era della magia nella capitale Tetai si diffuse una pestilenza dalla natura sconosciuta, essa portò gli abitanti a impazzire e sviluppare mutazioni diverse da quelle dei Pamorphi.</p>
   </div>
 );
 
 export const Page2 = () => (
   <div>
-    <h1>Pagina 2</h1>
-    <p>Testo segnaposto per la pagina 2.</p>
+    <h1>Pestilenza</h1>
+    <p>Secondo le parole della dottressa Maria Dalle Donne</p>
+    <p>  ...  </p>
+    <p>i medici che la stanno studiando hanno osservato come la sua diffusione sia simile a quella della peste nera diffusasi durante l’era dell’eresia di 350 anni fa. </p>
   </div>
 );
 
 export const Page3 = () => (
   <div>
-    <h1>Pagina 3</h1>
-    <p>Testo segnaposto per la pagina 3.</p>
+    <h1></h1>
+    <p>Nonostante sia risaputo che l’abito dei medici dell’epoca non li proteggesse veramente dal contagio, sembra che per la pestilenza funzionino molto efficacemente.</p>
+    <p>  ...  </p>
+    <p>Io e altri miei colleghi crediamo che si comporti in questo modo perché non si diffonde tramite mezzi tradizionali come virus o batteri, anche perche’ sembra che essa non si diffonda al di fuori di Tetai…</p>
   </div>
 );
 
 export const Page4 = () => (
   <div>
-    <h1>Pagina 4</h1>
-    <p>Testo segnaposto per la pagina 4.</p>
+    <h1>Effetti pestilenza</h1>
+    <p>Nei primi giorni dopo l’infezione, il soggetto comincia a riscontrare sbalzi di umore, forte prurito in alcune parti del corpo e una forte nausea.</p>
+    <p>  ...  </p>
+    <p>Dopo una enneamana, il soggetto comincia a farfugliare frasi senza alcun significato, punti in cui aveva prurito si sono ingrossate e gonfiate, si riscontra anche un aumento dell’aggressività.</p>
   </div>
 );
 
 export const Page5 = () => (
   <div>
-    <h1>Pagina 5</h1>
-    <p>Testo segnaposto per la pagina 5.</p>
+    <p>Dopo un mese il soggetto attacca a vista chiunque non sia affetto dalla pestilenza. I rigonfiamenti sul corpo si sono ingranditi diventando grossi tumori pulsanti da cui esce del pus infetto.</p>
+    <p>  ...  </p>
+    <p>Dopo diversi mesi alcuni soggetti raggiungono dimensioni notevoli, la forza fisica aumenta di conseguenza e sviluppano capacità paranormali impossibili da spiegare.</p>
   </div>
 );
 
 export const Page6 = () => (
   <div>
-    <h1>Pagina 6</h1>
-    <p>Testo segnaposto per la pagina 6.</p>
+    <h1>Chiesa e Catacombe</h1>
+    <p>La cattedrale della santa chiesa era il fulcro della religione dell’impero, ora divenuta il fulcro della pestilenza.</p>
+    <p>  ...  </p>
+    <p>Le sue catacombe ora pullulano di persone e mostri impazziti a causa della pestilenza che diventa più intensa man mano che si scende.</p>
   </div>
 );
 
 export const Page7 = () => (
   <div>
-    <h1>Pagina 7</h1>
-    <p>Testo segnaposto per la pagina 7.</p>
+    <p>È stato riscontrato un fenomeno inspiegabile, sembra che le catacombe abbiano preso vita, infatti cambiano conformazione periodicamente; il perché accada è sconosciuto.</p>
+    <p>  ...  </p>
+    <p>In ogni caso chiunque si addentri nelle catacombe ne fa ritorno come cadavere esposto all’esterno della cattedrale.</p>
   </div>
 );
 
 export const Page8 = () => (
   <div>
-    <h1>Pagina 8</h1>
-    <p>Testo segnaposto per la pagina 8.</p>
+    <h1>Pamorphi</h1>
+    <p>Essi sono umani mutati dalla magia, queste persone possono nascere con questa condizione o svilupparla durante la vita.</p>
+    <p>  ...  </p>
+    <p>La mutazione che li distingue sembra legata a creature del folklore dei luoghi in cui nascono o vivono.
+Queste persone non hanno nulla a che fare con la pestilenza.</p>
   </div>
 );
 
 export const Page9 = () => (
   <div>
-    <h1>Pagina 9</h1>
-    <p>Testo segnaposto per la pagina 9.</p>
+    <h1>Magia</h1>
+    <p>La magia arrivò il primo giorno del mese Violadasi caduto nel giorno Vialìd.</p>
+    <p>  ...  </p>
+    <p>Attualmente l’unica scienziata che sta studiando attivamente i suoi effetti è Violet di Scarlet.</p>
+    <p>  ...  </p>
+    <p>Non è chiaro se la magia e la pestilenza siano veramente connesse.</p>
   </div>
 );
 
 export const Page10 = () => (
-  <div>
-    <h1>Pagina 10</h1>
-    <p>Testo segnaposto per la pagina 10.</p>
-  </div>
+  <img
+    src={logoGif}
+    alt="Logo Animato"
+    style={{
+      position: 'fixed',
+      top: '50%',
+      left: '50%',
+      transform: 'translate(-50%, -50%)',
+      width: '80vw',
+      height: 'auto',
+      maxWidth: '100%',
+      maxHeight: '100%',
+    }}
+  />
 );
 
+
+
+//Leva dalle pale le pagine da qui in poi
 export const Page11 = () => (
   <div>
     <h1>Pagina 11</h1>
