79d8103dd0716231c8e7abf42eb5988fc016aec2
Cristian Pio Prudenzano <146067966+Cristian-Pio@users.noreply.github.com>
2025-05-30 17:08:54 +0200

    Pagina home

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import { Link } from 'react-router-dom'; // Importa Link per il routing
+import { Link } from 'react-router-dom';
 import '../styles/style.css';
+import '../styles/HomeStyle.css';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 
-// Importazione delle immagini
+// Importazione immagini
 import sidebarLeftImage from '../assets/right-sideBar-catacomb.png';
 import sidebarRightImage from '../assets/left-sideBar-catacomb.png';
+import titleImage from '../assets/The_Yellow_Abyss_game_Title_3.png';
+import steamBtn1 from '../assets/Steam button 1.png';
+import steamBtn2 from '../assets/Steam button 2.png';
 
 const HomePage = () => {
   return (
@@ -14,19 +18,50 @@ const HomePage = () => {
       <Navbar />
       <div className="main">
         <div className="left-sidebar">
-          <img src={sidebarLeftImage} alt="Sidebar Image" className="sidebar-img" />
+          <img src={sidebarLeftImage} alt="Sidebar Left" className="sidebar-img" />
         </div>
 
         <div className="main-content">
           <div className="content-title breadcrumb">
             <Link to="/" className="breadcrumb-current">Home</Link>
           </div>
-          <div className="content-title">content-title</div>
-          <div className="content-box">content-box</div>
+
+          {/* Titolo centrale */}
+          <div className="title-image-container">
+            <img src={titleImage} alt="Game Title" className="title-image" />
+          </div>
+
+          {/* Sezione video + descrizione */}
+          <div className="video-description-container">
+            <div className="video-box">
+            <iframe 
+              width="100%" 
+              height="315" 
+              src="https://www.youtube.com/embed/U0arvSdTXAc"
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe>
+            </div>
+            <div className="description-box">
+              <p>
+                Un cavaliere di nome Rosalind si addentra nelle catacombe della cattedrale della santa chiesa dell’impero, in cerca della sua amata Valerika scomparsa. 
+                Dovrà affrontare creature grottesche per raggiungere le profondità delle catacombe, ricevendo l'aiuto da un particolare vecchio signore vestito di giallo. 
+                I cittadini della citta' Tetai, capitale dell'impero, sono caduti sotto una profonda follia finendo per mutare in forme grottesche. 
+                L'epicentro di questa follia sono le catacombe della santa chiesa. Tutti coloro che hanno provato ad addentrarsi nelle catacombe sono ora esposti al suo ingresso come monito per i futuri avventurieri. 
+              </p>
+            </div>
+          </div>
+
+          {/* Bottone sotto */}
+          <div className="button-container">
+            <button className="steam-button"></button>
+          </div>
         </div>
 
         <div className="right-sidebar">
-          <img src={sidebarRightImage} alt="Sidebar Image" className="sidebar-img" />
+          <img src={sidebarRightImage} alt="Sidebar Right" className="sidebar-img" />
         </div>
       </div>
       <Footer />
