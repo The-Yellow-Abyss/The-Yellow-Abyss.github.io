3125ca7ba5849f49f50a73c2088ee1419fa3eaa6
gianlucamaggio21 <g.maggio21@studenti.uniba.it>
2025-06-10 11:37:38 +0200

    Video esplicativo embedded

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -85,7 +85,7 @@ const AboutPage = () => {
               <iframe
                 width="100%"
                 height="100%"
-                src="https://www.youtube.com/embed/ovdiGDEuoYU"
+                src="https://www.youtube.com/embed/C2zgA6FvGac"
                 title="YouTube video player"
                 frameBorder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
